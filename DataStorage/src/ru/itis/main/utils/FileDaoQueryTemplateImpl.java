4055fb967698a96b22fd1cf76579143454f458e0
Ayupov-Ayaz <ayupov.tom@gmail.com>
2017-05-01 17:21:50 +0300

    Create DeleteByValue in FileDaoQueryTemplateImpl

--- DataStorage/src/ru/itis/main/utils/FileDaoQueryTemplateImpl.java
+++ DataStorage/src/ru/itis/main/utils/FileDaoQueryTemplateImpl.java
@@ -37,12 +37,6 @@ public class FileDaoQueryTemplateImpl implements FileDaoQueryTemplate{
         return new ArrayList<T>();
     }
 
-    @Override
-    public <T> List<T> findByValue(String fileName, RowMapper<T> mapper, int valueColumn, Object value) {
-        //TODO: Реализовать findByValue
-       List<T> models = findAll(fileName,mapper);
-       return models;
-    }
 
     @Override
     public <T> int save(String fileName, T model) {
@@ -97,8 +91,73 @@ public class FileDaoQueryTemplateImpl implements FileDaoQueryTemplate{
     }
 
     @Override
-    public <T> void deleteByValue(String fileName, int valueColumn, RowMapper<T> mapper, Object value) {
-        //TODO: Реализовать deleteByValue
+    public <T> List<T> findByValue(String fileName, RowMapper<T> mapper, int valueColumn, Object value) {
+
+         List<T> models = new ArrayList<>();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+            String currentModelAsString = reader.readLine();
+            while(currentModelAsString != null){
+                Object castedValue = null;
+                String currentModelAsArray[] = currentModelAsString.split(" ");
+                String rowValue = currentModelAsArray[valueColumn];
+                if(value.getClass().getName().equals("java.lang.String")){
+                    castedValue = String.valueOf(rowValue);
+                }else if(value.getClass().getName().equals("java.lang.Integer")){
+                    castedValue = Integer.parseInt(rowValue);
+                }else if(value.getClass().getName().equals("java.lang.Double")){
+                    castedValue = Double.parseDouble(rowValue);
+                }else if(value.getClass().getName().equals("java.lang.Boolean")){
+                    castedValue = Boolean.valueOf(rowValue);
+                }
+                if(value.equals(castedValue)){
+                    T model = mapper.mapRow(currentModelAsString);
+                    models.add(model);
+                }
+                currentModelAsString = reader.readLine();
+
+            }
+
+                return models;
+        } catch (FileNotFoundException e) {
+            System.err.println("File not found Exception");
+        } catch (IOException e) {
+            System.err.println("IO Exception");
+        }
+
+        return models;
+    }
+    @Override
+    public void deleteByValue(String fileName, int valueColumn, Object value) {
+        try {
+            List<String> models = new ArrayList<>();
+            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+            String current = reader.readLine();
+            value = value.toString();
+            int finded = 0; // счетчик, что бы больше одного не удалить
+            while(current!=null){
+                String currenAsArray[] = current.split(" ");
+                String needElement=currenAsArray[valueColumn];
+                if(needElement.equals(value) && finded == 0){
+                    finded++;
+                }else{
+                    models.add(current);
+                }
+                current = reader.readLine();
+            }
+            // переписываем наш файл
+            BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));
+            for(int i = 0; i < models.size(); i++){
+                writer.write(models.get(i));
+                writer.newLine();
+            }
+            writer.close();
+        } catch (FileNotFoundException e) {
+            System.err.println("File not Found Exception!!");
+        } catch (IOException e) {
+            System.err.println("IO Exception =(");
+        }
+
 
     }
 }
