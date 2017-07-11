345ffb7652a3f9cc8e291b36fbaf6e34ca7401ac
Ayupov-Ayaz <ayupov.tom@gmail.com>
2017-07-11 18:01:57 +0300

    перенос стилей отдельно от html

--- chat/chat/src/main/java/ru/itis/config/PersistenceConfig.java
+++ chat/chat/src/main/java/ru/itis/config/PersistenceConfig.java
@@ -1,10 +1,4 @@
 package ru.itis.config;
-
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
